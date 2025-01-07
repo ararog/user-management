2a1450babd90980b11201c8990535cfaed34b92e
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-01-07 10:18:49 -0300

    Code cleanup.

--- src/app/helpers/api.ts
+++ src/app/helpers/api.ts
@@ -1,5 +1,5 @@
 
 export const apiClient = async (path: string, config?: RequestInit) => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
-  return await fetch(`${apiUrl}${path}`, config)
+  //const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+  return await fetch(path, config)
 }
